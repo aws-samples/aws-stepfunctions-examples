476ebcf1f9ecf47ed584119da18575ceb870799e
Justin <juscalli@amazon.com>
2021-03-22 08:11:44 -0700

    Updated to remove any non-standard libraries.

--- sam/app-decompose-for-parallelism/functions/get_test_results_from_state_machine_output/app.py
+++ sam/app-decompose-for-parallelism/functions/get_test_results_from_state_machine_output/app.py
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: MIT-0
 import json
 import sys
-import nested_lookup
 
 def lambda_handler(event, context):
 
@@ -21,35 +20,41 @@ def lambda_handler(event, context):
         "StateMachineArn": "",
         "TestName": "",
         "TestFaultMetricValue": 1,
-        "TestSuccessMetricValue": 0
+        "TestSuccessMetricValue": 0,
+        "test-run-id": "",
+        "Iteration": ""
     }
 
-    # Create a list of properties to look for in the event that was passed in
     idx = {
-        "TestStartTimeEpoch": ["StartDate",'','str'],
-        "TestEndTimeEpoch": ["StopDate",'','str'],
-        "TestStatus": ["Status",'','str'],
-        "StateMachineArn": ['StateMachineArn','','str'],
-        "Iteration": ["Name",'','str'],
+        "StartDate": ["TestStartTimeEpoch",'','str'],
+        "StopDate": ["TestEndTimeEpoch",'','str'],
+        "Status": ["TestStatus",'','str'],
+        "StateMachineArn": ["StateMachineArn",'','str'],
+        "Name": ["Iteration",'','str'],
         "test-run-id": ["test-run-id",'unknown','str']
     }
 
-    # For each of the properties in the lookup list, look recursively through the event provided
-    for k in idx:
-        print("Looking for {}".format(k))
-        k_info = idx[k]
-        try:
-            val = nested_lookup.nested_lookup(k_info[0],event)[0]
-            print("Found key {}".format(k_info[0]))
-            if k_info[2] == 'str':
-                val = str(val)
-            elif k_info[2] == 'int':
-                val = int(val)
-            res[k] = val
-        except:
-            print("Couldn't find key {} in event {}".format(k_info[0],event))
-            res[k] = k_info[1]
-    
+    list_to_process = [event]
+    while len(list_to_process) > 0:
+        this_item = list_to_process.pop(0)
+        if isinstance(this_item,dict):
+            for key in this_item:
+                #print("Looking at {} in {}".format(key,this_item))
+                if key in idx:
+                    k_info = idx[key]
+                    if res[k_info[0]] == '':
+                        val = this_item[key]
+                        #print("\t!!!Found key {}".format(key))                      
+                        if k_info[2] == 'str':
+                            val = str(val)
+                        elif k_info[2] == 'int':
+                            val = int(val)
+                        res[k_info[0]] = val
+                elif isinstance(this_item[key],dict):
+                    #print("\tValue is a dict, so adding to the list for further processing")
+                    list_to_process.append(this_item[key])
+
+
     # Update additional properties based on the ones extracted above
     res["TestDurationMs"] = str(int(res["TestEndTimeEpoch"]) - int(res["TestStartTimeEpoch"])  )  
     res["TestName"] = res["StateMachineArn"].split(':')[-1]
